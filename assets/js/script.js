270b91753019d674d82c7927c29421dd15925f17
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-06-11 17:40:58 +0100

    Refactor runGame function to accept game type and update game logic for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game loop, called when the script is first loadded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates 2 random bunbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+    }
+
 }
 
 function checkAnswer() {
@@ -43,8 +53,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
