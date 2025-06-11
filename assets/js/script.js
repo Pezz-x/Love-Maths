b881d6fa17147157730aeb5bf7b25b0ef79169ff
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-06-11 17:49:05 +0100

    Enhance calulateCorrectAnswer function to retrieve operands and operator, and return correct answer for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,9 +40,21 @@ function runGame(gameType) {
 function checkAnswer() {
 
 }
-
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus ect)
+ * directly from the dom, and returns the correct answer.
+ */
 function calulateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById(`operand1`).innerText);
+    let operand2 = parseInt(document.getElementById(`operand2`).innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+")
+        return [operand1 + operand2, "addition"]
+    else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `unimplemeted operator ${operator}. Apporting!`;
+    }
 }
 
 function incrementScore() {
@@ -57,7 +69,7 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById(`operand1`).textContent = operand1;
     document.getElementById(`operand2`).textContent = operand2;
     document.getElementById(`operator`).textContent = "+";
-    
+
 }
 
 function displaySubtractQuestion() {
